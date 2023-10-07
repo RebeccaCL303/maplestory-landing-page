1c1980f01147680ecb6a98826ea8f1c53e231284
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2023-10-06 20:50:54 -0500

    Revert "fixed warnings"
    This reverts commit 95c90feca618486d18fffefa265280426cf8549f.

--- src/Header.js
+++ src/Header.js
@@ -4,56 +4,56 @@ import "./Header.css";
 
 function Header() {
  return (
-  <header className="fixed-top">
-   <section className="disclaimer">
+  <header class="fixed-top">
+   <section class="disclaimer">
     <strong>Disclaimer: </strong>this is a coding project, I do not own any
     rights or services shown on this page.
    </section>
-   <div className="nav-top"></div>
-   <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
-    <div className="container-fluid">
-     <a className="navbar-brand" href="/">
+   <div class="nav-top"></div>
+   <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+    <div class="container-fluid">
+     <a class="navbar-brand" href="#">
       🍁Maplestory🍁
      </a>
      <button
-      className="navbar-toggler"
+      class="navbar-toggler"
       type="button"
       data-bs-toggle="collapse"
       data-bs-target="#navbarNav"
       aria-controls="navbarNav"
       aria-expanded="false"
       aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"></span>
      </button>
-     <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="primary-header">
+     <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="primary-header">
        <li>News</li>
        <li>The Game</li>
        <li>Community</li>
        <li>Support</li>
        <li>Buy Merch</li>
       </ul>
-      <ul className="social-header">
+      <ul class="social-header">
        <li>
-        <i className="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-facebook"></i>
        </li>
        <li>
-        <i className="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-twitter"></i>
        </li>
        <li>
-        <i className="fa-brands fa-youtube"></i>
+        <i class="fa-brands fa-youtube"></i>
        </li>
        <li>
-        <i className="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-instagram"></i>
        </li>
        <li>
-        <i className="fa-brands fa-twitch"></i>
+        <i class="fa-brands fa-twitch"></i>
        </li>
        <li>
-        <i className="fa-brands fa-discord"></i>
+        <i class="fa-brands fa-discord"></i>
        </li>
        <li>
-        <i className="fa-brands fa-tiktok"></i>
+        <i class="fa-brands fa-tiktok"></i>
        </li>
       </ul>
      </div>
@@ -64,7 +64,7 @@ function Header() {
      </button>
     </div>
    </nav>
-   <div className="nav-bottom"></div>
+   <div class="nav-bottom"></div>
   </header>
  );
 }
