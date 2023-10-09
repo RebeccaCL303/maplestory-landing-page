368720581dfeeee9ffddebc98096d94edc417f21
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2023-10-09 13:41:45 -0500

    added html and css for image slider

--- src/CurrentEvents.js
+++ src/CurrentEvents.js
@@ -5,11 +5,24 @@ import "./CurrentEvents.css";
 export default function CurrentEvents() {
  return (
   <div className="CurrentEvents">
-   <img
-    className="img-fluid"
-    src={require("./images/carnival-event.jpg")}
-    alt="nautilus carnival event"
-   />
+   <i class="fa-solid fa-chevron-left"></i>
+   <ul className="img-gallery">
+    <li>
+     <img
+      className="img-fluid"
+      src={require("./images/carnival-event.jpg")}
+      alt="nautilus carnival event"
+     />
+    </li>
+    <li>
+     <img
+      className="img-fluid"
+      src={require("./images/6th-star.jpg")}
+      alt="nautilus carnival event"
+     />
+    </li>
+   </ul>
+   <i class="fa-solid fa-chevron-right text-end"></i>
   </div>
  );
 }
