459eed89fe156cf00aeedf20e07dc418ac1b3d8a
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2023-10-20 15:02:15 -0500

    simplified code for cards in top row

--- src/Featured/Featured.js
+++ src/Featured/Featured.js
@@ -11,45 +11,32 @@ export default function Featured() {
     <ul className="row">
      <li className="col-6">
       <div className="card">
-       <div className="image">
-        <img src={carnival1} alt="news" />
-        <div className="label">FEATURED UPDATE</div>
-       </div>
-       <div className="description">
+       <img src={carnival1} alt="news" />
+       <div className="label">FEATURED UPDATE</div>
+       <div className="text">
         <h4>[Updated 10/4] v.245 - Midnight Carnival - Nautilus Patch Notes</h4>
         <p>
          Click here to read the patch notes for v.245 - Midnight Carnival -
          Nautilus.
         </p>
+        <p className="timestamp">6 Days Ago</p>
+        <p className="readmore">READ MORE</p>
        </div>
-       <p className="timestamp">6 Days Ago</p>
-       <p className="readmore">READ MORE</p>
       </div>
      </li>
      <li className="col-6">
       <div className="card">
-       <div className="image">
-        <img src={carnival1} alt="news" />
-        <div className="label">FEATURED UPDATE</div>
-       </div>
-       <div className="description">
+       <img src={carnival1} alt="news" />
+       <div className="label">FEATURED UPDATE</div>
+       <div className="text">
         <h4>[Updated 10/4] v.245 - Midnight Carnival - Nautilus Patch Notes</h4>
         <p>
          Click here to read the patch notes for v.245 - Midnight Carnival -
          Nautilus.
         </p>
+        <p className="timestamp">6 Days Ago</p>
+        <p className="readmore">READ MORE</p>
        </div>
-       <p className="timestamp">6 Days Ago</p>
-       <p className="readmore">READ MORE</p>
-      </div>
-     </li>
-    </ul>
-    <ul className="row">
-     <li className="col">
-      <div className="card">
-       <img src={carnival1} alt="news" />
-       <h4>event title</h4>
-       <p>description</p>
       </div>
      </li>
     </ul>
