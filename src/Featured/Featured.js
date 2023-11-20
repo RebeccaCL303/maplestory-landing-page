981763b33bd86eca652eb634892d5dafe6c469b0
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2023-11-20 14:50:40 -0600

    removed bootstrap grid styling

--- src/Featured/Featured.js
+++ src/Featured/Featured.js
@@ -8,86 +8,78 @@ export default function Featured() {
   <div className="Featured">
    <div className="featured-wrap">
     <h2>FEATURED NEWS AND UPDATES</h2>
-    <ul className="row">
-     <li className="col-6">
-      <div className="card card-sm">
-       <img src={carnival1} alt="news" />
-       <div className="label">FEATURED UPDATE</div>
-       <div className="text">
-        <h4>[Updated 10/4] v.245 - Midnight Carnival - Nautilus Patch Notes</h4>
+    <section>
+     <div className="card card-sm">
+      <img src={carnival1} alt="news" />
+      <div className="label">FEATURED UPDATE</div>
+      <div className="text">
+       <h4>[Updated 10/4] v.245 - Midnight Carnival - Nautilus Patch Notes</h4>
+       <p>
+        Cspanck here to read the patch notes for v.245 - Midnight Carnival -
+        Nautilus.
+       </p>
+       <div className="card-sm-bottom">
         <p>
-         Click here to read the patch notes for v.245 - Midnight Carnival -
-         Nautilus.
+         6 Days Ago <span>READ MORE</span>
         </p>
-        <div className="card-sm-bottom">
-         <p>
-          6 Days Ago <span>READ MORE</span>
-         </p>
-        </div>
        </div>
       </div>
-     </li>
-     <li className="col-6">
-      <div className="card card-sm">
-       <img src={carnival1} alt="news" />
-       <div className="label">FEATURED UPDATE</div>
-       <div className="text">
-        <h4>[Updated 10/4] v.245 - Midnight Carnival - Nautilus Patch Notes</h4>
+     </div>
+     <div className="card card-sm">
+      <img src={carnival1} alt="news" />
+      <div className="label">FEATURED UPDATE</div>
+      <div className="text">
+       <h4>[Updated 10/4] v.245 - Midnight Carnival - Nautilus Patch Notes</h4>
+       <p>
+        Cspanck here to read the patch notes for v.245 - Midnight Carnival -
+        Nautilus.
+       </p>
+       <div className="card-sm-bottom">
         <p>
-         Click here to read the patch notes for v.245 - Midnight Carnival -
-         Nautilus.
+         6 Days Ago <span>READ MORE</span>
         </p>
-        <div className="card-sm-bottom">
-         <p>
-          6 Days Ago <span>READ MORE</span>
-         </p>
-        </div>
        </div>
       </div>
-     </li>
-    </ul>
-    <ul className="row">
-     <li className="col-12">
-      <div className="card card-lg">
-       <div className="image">
-        <img src={carnival1} alt="news" className="img-fluid" />
-        <span className="label">EVENT</span>
-       </div>
-       <div className="text">
-        <h4>Community Livestream - Sixth Star Showdown w/ Team Pepe!</h4>
+     </div>
+    </section>
+    <section>
+     <div className="card card-lg">
+      <div className="image">
+       <img src={carnival1} alt="news" className="img-fluid" />
+       <span className="label">EVENT</span>
+      </div>
+      <div className="text">
+       <h4>Community livestream - Sixth Star Showdown w/ Team Pepe!</h4>
+       <p>
+        Join Community Manager Ezrabell alongside Team Pepe from the Sixth Star
+        Showdown event in a Community livestream!
+       </p>
+       <div className="card-lg-bottom">
         <p>
-         Join Community Manager Ezrabell alongside Team Pepe from the Sixth Star
-         Showdown event in a Community Livestream!
+         6 Days Ago <span>READ MORE</span>
         </p>
-        <div className="card-lg-bottom">
-         <p>
-          6 Days Ago <span>READ MORE</span>
-         </p>
-        </div>
        </div>
       </div>
-     </li>
-     <li className="col-12">
-      <div className="card card-lg">
-       <div className="image">
-        <img src={carnival1} alt="news" className="img-fluid" />
-        <span className="label">EVENT</span>
-       </div>
-       <div className="text">
-        <h4>Community Livestream - Sixth Star Showdown w/ Team Pepe!</h4>
+     </div>
+     <div className="card card-lg">
+      <div className="image">
+       <img src={carnival1} alt="news" className="img-fluid" />
+       <span className="label">EVENT</span>
+      </div>
+      <div className="text">
+       <h4>Community livestream - Sixth Star Showdown w/ Team Pepe!</h4>
+       <p>
+        Join Community Manager Ezrabell alongside Team Pepe from the Sixth Star
+        Showdown event in a Community livestream!
+       </p>
+       <div className="card-lg-bottom">
         <p>
-         Join Community Manager Ezrabell alongside Team Pepe from the Sixth Star
-         Showdown event in a Community Livestream!
+         6 Days Ago <span>READ MORE</span>
         </p>
-        <div className="card-lg-bottom">
-         <p>
-          6 Days Ago <span>READ MORE</span>
-         </p>
-        </div>
        </div>
       </div>
-     </li>
-    </ul>
+     </div>
+    </section>
    </div>
    <button className="p-3">
     <strong>SEE MORE NEWS</strong>
