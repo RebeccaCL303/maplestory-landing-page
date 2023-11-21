6aba2b342bbef136691537ea0c9f5ec7945469d8
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2023-11-21 16:10:39 -0600

    added community component and basic outline

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Header/Header.js";
 import CurrentEvents from "./CurrentEvents/CurrentEvents.js";
 import Featured from "./Featured/Featured.js";
+import Community from "./Community/Community.js";
 
 function App() {
  return (
@@ -10,6 +11,7 @@ function App() {
    <main>
     <CurrentEvents />
     <Featured />
+    <Community />
    </main>
   </div>
  );
---
+++ src/Community/Community.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Community.css";
+
+export default function Community() {
+ return (
+  <div className="Community">
+   <div className="community-wrap">
+    <h2>COMMUNITY NEWS</h2>
+    <div className="community-grid"></div>
+    <button>
+     SEE MORE
+     <br />
+     COMMUNITY NEWS
+    </button>
+   </div>
+  </div>
+ );
+}
