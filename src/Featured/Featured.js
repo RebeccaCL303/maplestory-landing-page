453d0e2db3cd52a4dab8b71c88fc5f6747c43fec
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2023-11-26 16:01:56 -0600

    Revert "created longer card in featured section"
    This reverts commit dafa3aef01c07237c3cb2ce3dbb7ace14c4d1a50.

--- src/Featured/Featured.js
+++ src/Featured/Featured.js
@@ -1,6 +1,5 @@
 import React from "react";
 import carnival1 from "../images/carnival-event.jpg";
-import art from "../images/art-corner.png";
 
 import "./Featured.css";
 
@@ -11,7 +10,7 @@ export default function Featured() {
     <h2>FEATURED NEWS AND UPDATES</h2>
     <div className="row">
      <div className="col-md-6">
-      <div className="card card-short">
+      <div className="card">
        <img src={carnival1} alt="news" />
        <div className="label">FEATURED UPDATE</div>
        <div className="text">
@@ -29,7 +28,7 @@ export default function Featured() {
       </div>
      </div>
      <div className="col-md-6">
-      <div className="card card-short">
+      <div className="card">
        <img src={carnival1} alt="news" />
        <div className="label">FEATURED UPDATE</div>
        <div className="text">
@@ -47,16 +46,42 @@ export default function Featured() {
       </div>
      </div>
     </div>
-    <div className="grid">
-     <div className="card-long">
-      <img src={art} alt="news" />
-      <span>
-       <h4>Check Out the November 2023 Art Corner</h4>
-       <p>Look at this month's roundup of our favorite fan art!</p>
-       <p>
-        6 Days Ago <span>READ MORE</span>
-       </p>
-      </span>
+    <div className="row">
+     <div className="col-md-6">
+      <div className="card">
+       <img src={carnival1} alt="news" />
+       <div className="label">FEATURED UPDATE</div>
+       <div className="text">
+        <h4>[Updated 10/4] v.245 - Midnight Carnival - Nautilus Patch Notes</h4>
+        <p>
+         Click here to read the patch notes for v.245 - Midnight Carnival -
+         Nautilus.
+        </p>
+        <div className="card-bottom">
+         <p>
+          6 Days Ago <span>READ MORE</span>
+         </p>
+        </div>
+       </div>
+      </div>
+     </div>
+     <div className="col-md-6">
+      <div className="card">
+       <img src={carnival1} alt="news" />
+       <div className="label">FEATURED UPDATE</div>
+       <div className="text">
+        <h4>[Updated 10/4] v.245 - Midnight Carnival - Nautilus Patch Notes</h4>
+        <p>
+         Click here to read the patch notes for v.245 - Midnight Carnival -
+         Nautilus.
+        </p>
+        <div className="card-bottom">
+         <p>
+          6 Days Ago <span>READ MORE</span>
+         </p>
+        </div>
+       </div>
+      </div>
      </div>
     </div>
    </div>
